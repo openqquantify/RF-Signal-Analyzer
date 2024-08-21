f35c309f50d0c94fd48f172ce4885b3b610ee1c2
t0x-1k <karnietheclown@yahoo.com>
2024-08-21 17:48:08 -0400

    Update Frequency_Capture.py
    update with 3D sim and obj file save

--- Frequency_Capture.py
+++ Frequency_Capture.py
@@ -1,49 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from rtlsdr import RtlSdr
+import pyvista as pv
+from pyvista import Plotter
 
-
+# All samples for SDR are hypothetical, these numbers must be changed to match use case
 sdr = RtlSdr()
-sdr.sample_rate = 2.048e6  # Set sample rate (depends on your device)
+sdr.sample_rate = 2.048e6  
 sdr.center_freq = 100e6    
 sdr.freq_correction = 60  
 sdr.gain = 'auto'         
 
+plotter = Plotter()
+plotter.set_background('black')
 
-def update_line(num, sdr, line):
-    samples = sdr.read_samples(256*1024)
+grid_size = 100
+z_scale = 1e-6
+x = np.linspace(-sdr.sample_rate / 2, sdr.sample_rate / 2, grid_size)
+y = np.linspace(0, 10, grid_size)
+
+grid = pv.StructuredGrid()
+grid.points = np.array(np.meshgrid(x, y, np.zeros(grid_size))).T.reshape(-1, 3)
+grid.dimensions = (grid_size, grid_size, 1)
+
+def update_plot():
+    samples = sdr.read_samples(256*1024) # Any real-time updating could be cpu intensive, just be aware
     fft_size = len(samples)
     window = np.blackman(fft_size)
     fft_result = np.fft.rfft(window * samples)
     fft_abs = np.abs(fft_result)
-    power_density = fft_abs**2 / fft_size
-    freqs = np.fft.rfftfreq(fft_size, d=1./sdr.sample_rate)
-    
-    x = np.linspace(-sdr.sample_rate/2, sdr.sample_rate/2, fft_size)
-    
-    line.set_ydata(power_density)  
-    return line,
+    power_density = 10 * np.log10(fft_abs**2 / fft_size)
 
-fig, ax = plt.subplots()
-line, = ax.plot([], [], lw=2)
+    z_values = np.tile(power_density[:grid_size] * z_scale, grid_size).reshape(grid.dimensions)
+    grid.points[:, 2] = z_values.flatten()
 
-
-ax.set_xlim(-sdr.sample_rate/2, sdr.sample_rate/2)
-ax.set_ylim(-100, -20)
-ax.grid()
-
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Power Density (dB)')
-plt.title('Real-time Spectrum Analysis')
-
+    plotter.update_coordinates(grid.points, render=True)
 
 try:
     while True:
-        update_line(0, sdr, line)
-        plt.pause(0.01)
-        plt.draw()
+        update_plot()
+        plotter.show(auto_close=False)
 except KeyboardInterrupt:
-    print("Interrupted by user, closing SDR...")
+    print("Interrupted by user, closing SDR")
 finally:
     sdr.close()
+    plotter.close()
 
+    # Save the grid as an OBJ file
+    grid.save("rf_capture.obj")
