d020a53166f4b159cff06b52818f786424273b34
t0x-1k <karnietheclown@yahoo.com>
2024-09-07 11:53:19 -0400

    Update frequency_simulation.py

--- frequency_simulation.py
+++ frequency_simulation.py
@@ -1,12 +1,18 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pyvista as pv
+import matplotlib.pyplot as plt
+
 
 power_watts = 100
 surface_area = 0.1
-clock_speed = 64e6
+clock_speed = 64e6 
 program_mem_size = 128000
 data_mem_size = 1536
+
+# Simulate 65% processor utilization
+utilization_factor = 0.65
+clock_speed *= utilization_factor
+power_watts *= utilization_factor 
 
 frequency_range = np.linspace(2e14, 4e14, 500)
 
@@ -15,7 +21,8 @@ power_density_sq = power_watts / surface_area
 gain = np.full(frequency_range.shape, 10)
 noise_level = np.sqrt(frequency_range) / 1000
 
-power_consumption = np.random.uniform(50, 200, size=frequency_range.shape)
+# Adjust power consumption based on processor utilization
+power_consumption = np.random.uniform(50, 200, size=frequency_range.shape) * utilization_factor
 
 device_levels = {
     "power_density_sq": power_density_sq,
@@ -29,25 +36,6 @@ device_levels = {
     "data_mem_size": data_mem_size
 }
 
-modulation_index = 0.5  
-message_signal_freq = 100e6  
-message_signal = np.sin(2 * np.pi * message_signal_freq * frequency_range / max(frequency_range))  
-am_modulated_signal = (1 + modulation_index * message_signal) * power_density_sq
-
-power_at_each_frequency = am_modulated_signal * gain - noise_level
-
-plt.figure(figure_size=(10, 6))
-plt.plot(frequency_range, power_at_each_frequency, label='Modulated Power')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Power Density')
-plt.title('Modulated Power Density vs Frequency')
-plt.legend()
-plt.grid(True)
-plt.show()
-
-plotter = pv.Plotter(notebook=True)
-plotter.set_background('black')
-
 x = np.linspace(0, 100, 20)
 y = np.linspace(0, 100, 20)
 z = np.linspace(0, 100, 20)
@@ -55,28 +43,35 @@ z = np.linspace(0, 100, 20)
 X, Y, Z = np.meshgrid(x, y, z)
 distances = np.sqrt(X**2 + Y**2 + Z**2)
 
-signal_strength = 1 / (distances + 1)  
+# Adjust signal strength calculation to account for processor utilization
+signal_strength = (1 / (distances + 1)) * utilization_factor  
 
 points = np.column_stack((X.flatten(), Y.flatten(), Z.flatten()))
 signal_strength_array = signal_strength.flatten()
 
-plotter.add_mesh(points, scalars=signal_strength_array, cmap="viridis", opacity=[0.0, 0.6])
-plotter.add_axes()
-plotter.show_grid()
 
-plotter.show()
+plotter = pv.Plotter(notebook=False)
+plotter.set_background('white')  # Set background for better contrast
 
-plotter.export_obj("rf_capture.obj")
+# Add mesh with larger points
+plotter.add_mesh(points, scalars=signal_strength_array, cmap="viridis", point_size=10, render_points_as_spheres=True, opacity=[0.0, 0.6])
 
-widget = pv.Plotter()
-widget.add_mesh(points, scalars=signal_strength_array, cmap="viridis", opacity=[0.0, 0.6])
-widget.add_axes()
-widget.show()
+# Set camera to overlook the scene, maybe this is the problem
+plotter.camera_position = [(150, 150, 150), (50, 50, 50), (0, 0, 1)]  
 
-iframe_widget = pv.Plotter()
-iframe_widget.add_mesh(points, scalars=signal_strength_array, cmap="viridis", opacity=[0.0, 0.6])
-iframe_widget.add_axes()
-iframe_widget.show()
 
-print(widget)
-print(iframe_widget)
+plotter.show(auto_close=False)
+
+
+depth_image = plotter.get_image_depth()
+
+
+plotter.close()
+
+plt.figure(figsize=(10, 8))
+plt.imshow(depth_image, cmap='gray')
+plt.colorbar(label='Depth')
+plt.title('Depth Image')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.show()
